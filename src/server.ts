9e1b7f95cb654a7867582428326d5ed667c48fba
EnjoylifeUBT <you@example.com>
2020-04-02 15:57:36 +0100

    first edition

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import WebSocket from 'ws';
+const wss = new WebSocket.Server({ port: 5001 });
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    console.log('....' + message);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws) {
+        client.send(message);
+      }
+    });
+    //ws.send('....' + message);
+  });
+  ws.on('close', function() {
+    console.log('One of clients leaves the communication');
+  });
+  console.log('New client connected');
+});
